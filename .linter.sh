80353c3febb92fcb0dbd640cfd7c81491a39f1a5
Gerardo Rios <gerardo@kavia.ai>
2025-05-30 16:37:30 -0500

    Auto-commit: Changes as of 2025-05-30T21:37:30.411421+00:00

---
+++ .linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /tmp/kavia/workspace/code-generation/memorymatch-flutter-614029-242573df/memorymatch_container
+flutter analyze
+
---
+++ memorymatch_container/android/app/src/main/kotlin/com/example/memorymatch_container/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.memorymatch_container
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
